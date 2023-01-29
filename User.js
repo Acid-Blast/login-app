e8440ef3c8a7d05829eebc539a1b28cb27634b25
Acid-Blast <marcelo.fa.lasca@hotmail.com>
2023-01-29 18:06:51 -0300

    first commit

---
+++ User.js
@@ -0,0 +1,7 @@
+class User{
+    constructor(obj){
+        this._user = obj._user;
+        this._mail = obj._mail;
+        this._pass = obj._pass;
+    }
+}
---
+++ login.js
@@ -0,0 +1,145 @@
+/*
+PreEntrega 3
+Marcelo Falasca
+
+Login:
+    El sistema compara el usuario y contraseña ingresado contra los datos almacenados en el localStorage.
+    Si el usuario no existe, no entra.
+    Cuenta con la opcion de registrarse para guardar al usuario en el localStorage
+    Mientras cumpla que:
+        - No existe un usuario con el mismo nombre
+        - El mail ingresado no esté registrado con otro usuario
+        - Y que coincidan las contraseñas ingresadas
+*/
+
+
+//cargo en una variable el contenido del Local Storage
+let BBDD = JSON.parse(localStorage.getItem("users"));
+if(BBDD == null) BBDD = [];
+console.log(BBDD);
+
+
+//registro de usuario nuevo
+const btnRegistro = document.getElementById("btn-register");
+btnRegistro.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    const dialog = document.querySelector("#dialogReg");
+    const salir = document.getElementById("salir");
+    
+    //abre el modal de registro
+    dialog.showModal();
+    salir.addEventListener("click", () => {
+        dialog.close();
+    });
+
+    //para que el modal se cierre si se clickea afuera
+    window.onclick = (e) => {
+        if (e.target == dialog) {
+          dialog.close();
+        }
+    }
+    
+    //envia los datos cargados para validar la creacion del usuario
+    const formReg = document.getElementById("formReg");
+    formReg.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        const user = document.getElementById("userReg");
+        const mail = document.getElementById("mailReg");
+        const pass = document.getElementById("passReg");
+        const pass2 = document.getElementById("pass2Reg");
+    
+        if(validarUsuario(user.value, mail.value, pass.value, pass2.value)){
+            guardarUsuario(user.value, mail.value, pass.value);
+            dialog.close();
+            user.value= "";
+            mail.value= "";
+            pass.value= "";
+            pass2.value= "";
+        }
+    });
+
+});
+
+
+//devuelve false si no se valida la creacion y true si es valida
+const validarUsuario = (user, mail, pass, pass2) => {
+    if(BBDD.find(e => e._user === user) != undefined){
+        createModal("Error en registro", ` El usuario "${user}" ya está registrado`);
+        return false;
+    }
+    if(BBDD.find(e => e._mail === mail) != undefined){
+        createModal("Error en registro", ` Ya existe una cuenta con el correo: "${mail}"`);
+        return false;
+    }
+    if(pass != pass2){
+        createModal("Error en registro", " Las contraseñas no coinciden");
+        return false;
+    } 
+    
+    createModal("Nuevo Usuario", `
+        Usuario creado con exito <br>
+        Nombre: <i>${user}</i> <br>
+        Correo: <i>${mail}</i> 
+    `)
+    return true;
+    
+}
+
+//crea un modal y lo muestra
+const createModal = (titulo, mensaje) => {
+    const modal = document.getElementById("dialog-alert");
+    modal.close();
+    window.onclick = (e) => {
+        if (e.target == modal) {
+          modal.close();
+         }
+    }
+
+    modal.innerHTML = `
+            <h2> ${titulo} </h2>
+            <h3> ${mensaje} </h3>
+            <button id="btn-salir"> X </button>
+        `;
+
+    modal.showModal()
+    const salir = document.getElementById("btn-salir");
+    salir.addEventListener("click", () => {
+        modal.removeAttribute(open);
+        modal.close();
+    });
+}
+
+//agrega el usuario al storage mientras no sea un string vacio o null
+const guardarUsuario = (user, mail, pass) => {
+    BBDD.push(new User({_user: user, _mail: mail, _pass: pass}));
+    localStorage.setItem("users", JSON.stringify(BBDD));
+    console.log(BBDD);
+}
+
+//login
+const miForm = document.getElementById("form");
+miForm.addEventListener("submit", (e) => {
+
+    const user = document.getElementById("user");
+    const pass = document.getElementById("pass");
+
+    //compara los datos ingresados con el Local Storage, si no existen, previene la accion del form, si existe, inicia sesion
+    let found = BBDD.find(e => (e._user === user.value && e._pass === pass.value));
+    if(found == undefined){
+        e.preventDefault();
+        alert(`Usuario o contraseña incorrectos`);
+    }else{
+        alert("Iniciando sesion");
+    }
+});
+
+//boton de info
+const info = document.getElementById("infoIcon");
+info.addEventListener("click", () => {
+    createModal("Info", `
+    Alumno: <i>Marcelo Falasca</i> <br><br>
+    Proximamente este Login va a dar entrada a la App del proyecto final.
+    `);
+});
